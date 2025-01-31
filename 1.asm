d8498f76e4a47410f42fb12c135f452ea527b64a
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-01-31 20:10:20 +0545

    first commit

---
+++ 1.asm
@@ -0,0 +1,5 @@
+; ; f = (g +h) - (i + j)
+; let f= x1, g = x2, h = x3 , i = x4 and j = x5
+add x6, x2, x3
+add x7, x4, x5
+sub x1, x6, x7 
---
+++ README.md
@@ -0,0 +1,3 @@
+# RISC-V
+
+> This repo contains risc-v assembly programs written to learn computer architecture and assembly.
